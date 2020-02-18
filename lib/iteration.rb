974fe20a85ed664a7d571bd7da207235629d39ae
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-18 21:52:34 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -48,9 +48,9 @@ def total_even_pairs(src)
 total = 0
 row_index = 0
   while row_index < src.count do
-    if src[row_index][0]%2 == 0
-      if src[row_index][1]%2 == 0
-        total+= (src[row_index][0] + src[row_index][1])
+    if src[row_index][0] % 2 == 0
+      && src[row_index][1] % 2 == 0
+      then total+= (src[row_index][0] + src[row_index][1])
     end
   end
   row_index+=1
